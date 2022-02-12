8c3c14a4443a359e449807d5223c154014992914
tanc <tanc.geant@gmail.com>
2022-02-12 18:03:12 +0100

    Fix index

--- back/src/api/index.js
+++ back/src/api/index.js
@@ -2,6 +2,8 @@ import { Router } from 'express'
 import user from './user'
 import auth from './auth'
 import passwordReset from './password-reset'
+import campaign from './campaign'
+import pak from './pak'
 
 const router = new Router()
 
@@ -31,5 +33,7 @@ const router = new Router()
 router.use('/users', user)
 router.use('/auth', auth)
 router.use('/password-resets', passwordReset)
+router.use('/campaigns', campaign)
+router.use('/paks', pak)
 
 export default router
