ccd9e1122c49a6e1177ef9f1754d23b996192a71
tanc <tanc.geant@gmail.com>
2022-02-14 15:22:36 +0100

    Add front

---
+++ front/sitac-webapp/src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="app">
+    <link
+      rel="stylesheet"
+      href="https://cdn.materialdesignicons.com/5.3.45/css/materialdesignicons.min.css"
+    />
+    <Header/>
+    <div id="nav">
+      <router-link to="/">Connexion</router-link>
+    </div>
+    <router-view />
+  </div>
+</template>
+
+
+<style>
+#nav {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  padding: 10px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+
+<script>
+import Header from './components/Header.vue';
+//import AdminHeader from "./components/AdminHeader.vue";
+//import SideMenu from "./components/SideMenu.vue";
+export default {
+  name: "App",
+  components: {
+    Header
+    // SideMenu,
+  },
+};
+</script>
---
+++ front/sitac-webapp/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="hero is-primary">
+  <div class="hero-body">
+    <p class="title">
+      SITAC
+    </p>
+    <p class="subtitle">
+      Système Informatique Tactique d'Assault Coordonné
+    </p>
+  </div>
+</section>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
