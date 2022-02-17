e3f682f7fb9acf8164ec827802bc60b6dec4860f
tanc <tanc.geant@gmail.com>
2022-02-17 16:16:18 +0100

    Add table API

---
+++ front/sitac-webapp/src/components/TableAPI.vue
@@ -0,0 +1,178 @@
+<template>
+  <div>
+    <b-field label="Recherche" label-position="on-border">
+      <b-input
+        placeholder="Nom"
+        type="search"
+        v-model="searchQuery"
+        @keyup.native.enter="refresh"
+        expanded
+      ></b-input>
+      <p class="control">
+        <b-button class="button is-primary" @click="refresh"
+          >Rechercher</b-button
+        >
+      </p>
+    </b-field>
+    <section>
+      <b-button type="is-primary" :loading="loading" @click="refresh"
+        >Rafraichir</b-button
+      >
+    </section>
+    <section>
+      <b-table
+        :data="data"
+        :loading="loading"
+        paginated
+        paginationPosition="top"
+        backend-pagination
+        :total="total"
+        :per-page="perPage"
+        @page-change="onPageChange"
+        aria-next-label="Next page"
+        aria-previous-label="Previous page"
+        aria-page-label="Page"
+        aria-current-label="Current page"
+        backend-sorting
+        :default-sort-direction="defaultSortOrder"
+        :default-sort="[sortField, sortOrder]"
+        @sort="onSort"
+        @click="onItemClick"
+      >
+        <b-table-column field="name" label="Name" sortable v-slot="props">
+          {{ props.row.name }}
+        </b-table-column>
+
+        <b-table-column field="description" label="Description" sortable v-slot="props">
+          {{ props.row.description }}
+        </b-table-column>
+
+        <b-table-column
+          field="createdAt"
+          label="Date"
+          sortable
+          centered
+          v-slot="props"
+        >
+          {{
+            props.row.createdAt
+              ? new Date(props.row.createdAt).toLocaleDateString() +
+                " " +
+                new Date(props.row.createdAt).toLocaleTimeString()
+              : "unknown"
+          }}
+        </b-table-column>
+      </b-table>
+    </section>
+  </div>
+</template>
+
+
+
+<script>
+import { ToastProgrammatic as Toast } from "buefy";
+export default {
+  props: ["url", "accessToken"],
+  data() {
+    return {
+      data: [],
+      total: 0,
+      loading: false,
+      sortField: "createdAt",
+      sortOrder: "desc",
+      defaultSortOrder: "desc",
+      page: 1,
+      perPage: 10,
+      searchQuery: "",
+    };
+  },
+  methods: {
+    /*
+     * Load async data
+     */
+    loadAsyncData() {
+      const params = [
+        `page=${this.page}`,
+        `access_token=${this.accessToken}`,
+        `limit=${this.perPage}`,
+        `sort=${this.sortOrder == "desc" ? "-" : ""}${this.sortField}`,
+        `${this.searchQuery != "" ? "q=" : ""}${this.searchQuery}`,
+      ].join("&");
+      this.loading = true;
+      this.$http
+        .get(`${this.url}/campaigns?${params}`)
+        .then(({ data }) => {
+          this.data = [];
+          let oneMorePage = 10 * this.page + 10;
+          let fullPage = 10 * this.page;
+          let currentItems = data.length * this.page;
+          console.log("fullPage: " + fullPage);
+          console.log("currentItems: " + currentItems);
+          if (this.total < oneMorePage && currentItems == fullPage) {
+            this.total = oneMorePage;
+          }
+          data.forEach((item) => {
+            this.data.push(item);
+          });
+          this.loading = false;
+        })
+        .catch((error) => {
+          Toast.open({
+            message: "Vous n'êtes pas connecté à votre compte.",
+            type: "is-danger",
+          });
+          this.data = [];
+          this.total = 0;
+          this.loading = false;
+          throw error;
+        });
+    },
+    /*
+     * Handle page-change event
+     */
+    onPageChange(page) {
+      this.page = page;
+      this.loadAsyncData();
+    },
+    /*
+     * Handle sort event
+     */
+    onSort(field, order) {
+      this.sortField = field;
+      this.sortOrder = order;
+      this.total = 0;
+      this.loadAsyncData();
+    },
+    /*
+     * Type style in relation to the value
+     */
+    type(value) {
+      const number = parseFloat(value);
+      if (number < 6) {
+        return "is-danger";
+      } else if (number >= 6 && number < 8) {
+        return "is-warning";
+      } else if (number >= 8) {
+        return "is-success";
+      }
+    },
+    refresh() {
+      this.loadAsyncData();
+    },
+    /*onItemClick(row) {
+      this.$router.push("detailPP/" + row.id);
+    },*/
+  },
+  filters: {
+    /**
+     * Filter to truncate string, accepts a length parameter
+     */
+    truncate(value, length) {
+      return value.length > length ? value.substr(0, length) + "..." : value;
+    },
+  },
+  mounted() {
+    this.loadAsyncData();
+  },
+};
+</script>
